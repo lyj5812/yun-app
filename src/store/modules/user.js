46344649880ddf1c3c885b542f79ce6b36972ea6
lyj <1264415695@qq.com>
2020-07-01 00:04:30 +0800

    完善

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,87 +1,86 @@
-import {wxMalogin, getWxMiniUserInfo} from '@/api/user'
-import {setToken, getToken} from '@/common/auth'
+import { wxMalogin, getWxMiniUserInfo } from '@/api/user'
+import { setToken } from '@/common/auth'
 
 const state = {
-    token: getToken(),
-    name: '',
-    avatar: ''
+  token: '',
+  name: '',
+  avatar: ''
 }
 
 const mutations = {
-    SET_TOKEN: (state, token) => {
-        state.token = token
-    },
-    SET_NAME: (state, name) => {
-        state.name = name
-    },
-    SET_AVATAR: (state, avatar) => {
-        state.avatar = avatar
-    }
+  SET_TOKEN: (state, token) => {
+    state.token = token
+  },
+  SET_NAME: (state, name) => {
+    state.name = name
+  },
+  SET_AVATAR: (state, avatar) => {
+    state.avatar = avatar
+  }
 }
 
 const actions = {
-    // 登录
-    login({commit}, userInfo) {
-        return new Promise((resolve, reject) => {
-            // #ifdef  MP-WEIXIN
-            uni.login({
-                provider: 'weixin',
-                success: function (loginRes) {
-                    console.log(loginRes);
-                    //微信小程序用户信息
-                    uni.getUserInfo({
-                        provider: 'weixin',
-                        success: function (infoRes) {
-                            wxMalogin({code: loginRes.code,encryptedData: infoRes.encryptedData,iv:infoRes.iv}).then((res) => {
-                                const {data} = res
-                                commit('SET_TOKEN', data.access_token)
-                                setToken(data.access_token, data.expires_in * 1000)
-                                resolve()
-                            })
-                        }
-                    });
+  // 登录
+  login({ commit }, userInfo) {
+    return new Promise((resolve, reject) => {
+      // #ifdef  MP-WEIXIN
+      uni.login({
+        provider: 'weixin',
+        success: function(loginRes) {
+          console.log(loginRes)
+          // 微信小程序用户信息
+          uni.getUserInfo({
+            provider: 'weixin',
+            success: function(infoRes) {
+              wxMalogin({ code: loginRes.code, encryptedData: infoRes.encryptedData, iv: infoRes.iv }).then((res) => {
+                const { data } = res
+                commit('SET_TOKEN', data.access_token)
+                setToken(data.access_token, data.expires_in * 1000)
+                resolve()
+              })
+            }
+          })
+        },
+        fail: function(e) {
+          reject(e)
+        }
+      })
+      // #endif
+      console.log(userInfo)
+    })
+  },
 
-                },
-                fail: function (e) {
-                    reject(e)
-                }
-            });
-            // #endif
-            console.log(userInfo)
-        })
-    },
-
-    // 获取用户信息
-    getInfo({commit}) {
-        return new Promise((resolve, reject) => {
-            // #ifdef  MP-WEIXIN
-            //微信小程序用户信息
-            getWxMiniUserInfo().then(response => {
-                const data = response.data.data
-                if (!data) {
-                    reject('error')
-                }
-                commit('SET_NAME',data.nickName)
-                commit('SET_AVATAR',data.avatarUrl)// 头像
-                resolve(data)
-            }).catch(error => {
-                reject(error)
-            })
-            // #endif
-        })
-    }
+  // 获取用户信息
+  getInfo({ commit }) {
+    return new Promise((resolve, reject) => {
+      // #ifdef  MP-WEIXIN
+      // 微信小程序用户信息
+      getWxMiniUserInfo().then(response => {
+        const data = response.data.data
+        if (!data) {
+          reject('error')
+        }
+        commit('SET_NAME', data.nickName)
+        commit('SET_AVATAR', data.avatarUrl)// 头像
+        resolve(data)
+      }).catch(error => {
+        reject(error)
+      })
+      // #endif
+    })
+  }
 }
 
 const getters = {
-    name: state => state.name,
-    avatar: state => state.avatar,
-    token: state => state.token
+  name: state => state.name,
+  avatar: state => state.avatar,
+  token: state => state.token
 }
 
 export default {
-    namespaced: true,
-    state,
-    mutations,
-    actions,
-    getters
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+  getters
 }
